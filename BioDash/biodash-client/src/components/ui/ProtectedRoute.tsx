0a6d44844309a82182ea7ccbb0d80923768a156c
lohjs <lohane.mdev@gmail.com>
2026-04-26 16:34:23 -0300

    Update ProtectedRoute.tsx

--- BioDash/biodash-client/src/components/ui/ProtectedRoute.tsx
+++ BioDash/biodash-client/src/components/ui/ProtectedRoute.tsx
@@ -1,7 +1,26 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthStore } from '../../store/authStore'
+import { useEffect, useState } from 'react'
 
 export default function ProtectedRoute() {
-    const isAuthenticated = useAuthStore((s) => s.isAuthenticated())
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated())
+  const [hydrated, setHydrated] = useState(false)
+
+  useEffect(() => {
+    const unsub = useAuthStore.persist.onFinishHydration(() => {
+      setHydrated(true)
+    })
+
+    if (useAuthStore.persist.hasHydrated()) {
+      setHydrated(true)
+    }
+
+    return () => unsub()
+  }, [])
+
+  if (!hydrated) {
+    return null
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />
 }
